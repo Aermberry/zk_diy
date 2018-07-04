4456cb653f8b34b872f262b3b0bd4c26fcce3146
sampsong <sampsong@qq.com>
2018-07-04 21:18:37 +0800

    删除之前代码

--- src/service/api/config.js
+++
@@ -1,9 +0,0 @@
-const { CLIENT_URL, CLIENT_ID, CLIENT_KEY } = process.env
-
-console.log(process.env)
-
-export default {
-  url: CLIENT_URL || 'http://admin.czhait.com/',
-  id: CLIENT_ID || 'test_id',
-  key: CLIENT_KEY || 'test_key'
-}
