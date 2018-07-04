da21758632545d31d5be883bd1ef76263abaa057
sampsong <sampsong@qq.com>
2018-07-05 07:24:19 +0800

    修改inport

--- src/main.js
+++ src/main.js
@@ -5,11 +5,10 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
-import {
-  api
-} from '@/service/api'
+
 import '@/styles/index.scss' // global css
 
+import global from '@/service/core/global' // 公共函数,全局函数
 import App from './App'
 import router from './router'
 import store from './store'
@@ -17,8 +16,8 @@ import XIcon from '@/elements/x-icon'
 import XScreenfull from '@/elements/x-screenfull'
 
 import '@/permission' // permission control
+Vue.use(global)
 
-Vue.prototype.$api = api
 Vue.use(ElementUI, {
   locale
 })
