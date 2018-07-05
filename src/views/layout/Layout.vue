e461fdd9915b30301873e3c3338fc44cd0b60d75
sampsong <sampsong@qq.com>
2018-07-05 20:59:22 +0800

    更改后台URL路径

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -38,7 +38,8 @@
     },
     methods: {
       async init () {
-        this.diyUrl = 'http://www.yiqipingou.com/'
+        // this.diyUrl = 'http://www.yiqipingou.com/'
+        this.diyUrl = 'http://localhost:2000/pages/index'
       }
 
     }
@@ -48,36 +49,36 @@
 <style rel="stylesheet/scss" lang="scss" scoped>
   @import 'src/assets/styles/mixin.scss';
   .app-wrapper {
-    @include clearfix;
-    position: relative;
-    height: 100%;
-    width: 100%;
-    padding-left: 80px;
-    display: flex;
-    .iphone-x {
-      // z-index: -1;
-      // background: url(./../../assets/img/iphonex.png) no-repeat;
-      border:10xp solid red;
-      border-top-width: 30px;
-      border-image: url(./../../assets/img/iphonex.png);
-      // background-size: 380px;
-      transform: translate(-250%, 15%);
-      display: flex;
-      width: 490px;
-      // height: 200px;
-      border-radius: 30px;
-      // background: #008080;
-      overflow: hidden;
-      height: 726px;
-      position: relative;
+  	@include clearfix;
+  	position: relative;
+  	height: 100%;
+  	width: 100%;
+  	padding-left: 80px;
+  	display: flex;
+  	.iphone-x {
+  		// z-index: -1;
+  		// background: url(./../../assets/img/iphonex.png) no-repeat;
+  		border: 10xp solid red;
+  		border-top-width: 30px;
+  		border-image: url(./../../assets/img/iphonex.png);
+  		// background-size: 380px;
+  		transform: translate(-250%, 15%);
+  		display: flex;
+  		width: 490px;
+  		// height: 200px;
+  		border-radius: 30px;
+  		// background: #008080;
+  		overflow: hidden;
+  		height: 726px;
+  		position: relative;
 
-      .show-iframe {
-        // position: absolute;
-        width: 480px;
-        // margin-left: 0px;
-        overflow-x: hidden;
-        overflow-y: auto;
-      }
-    }
+  		.show-iframe {
+  			// position: absolute;
+  			width: 480px;
+  			// margin-left: 0px;
+  			overflow-x: hidden;
+  			overflow-y: auto;
+  		}
+  	}
   }
 </style>
