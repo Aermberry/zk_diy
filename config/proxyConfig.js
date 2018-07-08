bcafbf295823d56aa6a8aecc883a65995f9e0af7
sampsong <sampsong@qq.com>
2018-07-08 15:36:31 +0800

    更新代码跨域

---
+++ config/proxyConfig.js
@@ -0,0 +1,25 @@
+// module.exports = {
+//   proxyList: {
+//     '/webs': {
+//       // 测试环境
+//       target: 'http://localhost:2000/', // 接口域名
+//       changeOrigin: true, //是否跨域
+//       pathRewrite: {
+//         '^/index': 'pages/index' //需要rewrite重写的,
+//       }
+//     }
+//   }
+// }
+
+module.exports = {
+  proxyList: {
+    '/webs': {
+      // 测试环境
+      target: 'https://www.baidu.com/', // 接口域名
+      changeOrigin: true, //是否跨域
+      pathRewrite: {
+        '^/index': 'index.html' //需要rewrite重写的,
+      }
+    }
+  }
+}
