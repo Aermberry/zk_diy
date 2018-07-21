3237d4c02be748158db6cbb805dc6fd2030640b2
zhongku <2839018@qq.com>
2018-07-21 11:19:11 +0800

    提交main.js

--- src/main.js
+++ src/main.js
@@ -1,33 +1,45 @@
 import Vue from 'vue'
-
-import ElementUI from 'element-ui'
-import '@/assets/styles/theme-src/index.scss' // 引入element-ui，尽可能不去修改
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
-import '@/assets/styles/index.scss' // global css
-
-import global from '@/service/core/global' // 公共函数,全局函数
 import App from './App'
 import router from './router'
-import XIcon from '@/elements/x-icon'
-import XScreenfull from '@/elements/x-screenfull'
+import local from '@/service/core/local'
+import global from '@/service/core/global'
+import locale from 'element-ui/lib/locale/lang/en'
+import ElementUI from 'element-ui'
+import '@/assets/styles/theme-src/index.scss'
+import '@/assets/styles/index.scss'
+import {
+  api
+} from '@/service/api'
+
+import ZkAutoForm from '@/components/zk-auto-form'
 import ZkFile from '@/components/zk-file'
+import ZkSafeColor from '@/components/zk-safe-color'
 import ZkTable from '@/components/zk-table'
 import ZkWidgetData from '@/components/zk-widget-data'
-import ZkAutoForm from '@/components/zk-auto-form'
+
+import XIcon from '@/elements/x-icon'
+import XScreenfull from '@/elements/x-screenfull'
+
+Vue.component('zk-auto-form', ZkAutoForm)
+Vue.component('zk-file', ZkFile)
+Vue.component('zk-safe-color', ZkSafeColor)
+Vue.component('zk-table', ZkTable)
+Vue.component('zk-widget-data', ZkWidgetData)
+
+Vue.component('x-icon', XIcon)
+Vue.component('x-screenfull', XScreenfull)
 
 Vue.use(global)
-
 Vue.use(ElementUI, {
   locale
 })
-Vue.component('x-icon', XIcon)
-Vue.component('x-screenfull', XScreenfull)
-Vue.component('zk-table', ZkTable)
-Vue.component('zk-file', ZkFile)
-Vue.component('zk-widget-data', ZkWidgetData)
-Vue.component('zk-auto-form', ZkAutoForm)
-Vue.config.productionTip = false
 
+Vue.config.productionTip = false
+Vue.prototype.$api = api
+Vue.prototype.$local = local
+Vue.prototype.$toast = local
+Vue.prototype.$message = local
+Vue.prototype.$loading = true
 // eslint-disable-next-line
 new Vue({
   el: '#app',
@@ -35,10 +47,3 @@ new Vue({
   render: h => h(App)
 })
 
-// new Vue({
-//   router,
-//   components: {
-//     App
-//   },
-//   template: '<App/>'
-// }).$mount('#zkcloud')
