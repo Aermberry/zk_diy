31f19aba7f63c6f751f8cebddc85f8ab2dbfdc5f
zhongku <2839018@qq.com>
2018-07-21 11:29:28 +0800

    修改路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -14,63 +14,15 @@ export const constantRouterMap = [{
   {
     path: '/',
     component: Layout,
-    redirect: '/dashboard',
-    name: 'Dashboard',
+    redirect: '/index',
+    name: 'index',
     hidden: true,
     children: [{
-      path: 'dashboard',
-      component: () =>
-        import('@/views/dashboard/index')
-    }]
-  },
-
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: {
-      title: 'Example',
-      icon: 'example'
-    },
-    children: [{
-        path: 'table',
-        name: 'Table',
-        component: () =>
-          import('@/views/table/index'),
-        meta: {
-          title: 'Table',
-          icon: 'table'
-        }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () =>
-          import('@/views/tree/index'),
-        meta: {
-          title: 'Tree',
-          icon: 'tree'
-        }
-      }
-    ]
-  },
-
-  {
-    path: '/form',
-    component: Layout,
-    children: [{
       path: 'index',
-      name: 'Form',
       component: () =>
-        import('@/views/form/index'),
-      meta: {
-        title: 'Form',
-        icon: 'form'
-      }
+        import('@/views/index')
     }]
   },
-
   {
     path: '*',
     redirect: '/404',
