a992f5549a3e6051c91cb95493364f15a3da166c
zhongku <2839018@qq.com>
2018-07-21 22:05:25 +0800

    文件管理窗口优化

---
+++ src/components/zk-file-icon/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="zk-file-icon"  v-if="asyncflag">
+ 
+  </div>
+</template>
+
+<script>
+  import { THEME_GETVALUE_GET } from '@/service/api/apiUrl'
+  export default {
+    name: 'zk-file-icon',
+    props: {
+      widget: {}
+    },
+    data () {
+      return {
+        viewModel: '',
+        asyncflag: false
+      }
+    },
+    mounted () {
+      this.init()
+    },
+    methods: {
+      async  init () {
+        this.asyncflag = true
+        const parameter = {
+          dataId: this.widget && this.widget.dataId,
+          defaultId: '5b406cddfef00000a0000001'
+        }
+        this.viewModel = await this.$api.get(THEME_GETVALUE_GET, parameter)
+        // console.info('zk-file-icon数据',this.viewModel)
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import './style.scss';
+</style>
+
--- src/main.js
+++ src/main.js
@@ -14,10 +14,16 @@ import {
 import ZkAutoForm from '@/components/zk-auto-form'
 import ZkDialog from '@/components/zk-dialog'
 import ZkFile from '@/components/zk-file'
+import ZkFileAudio from '@/components/zk-file-audio'
+import ZkFileCloud from '@/components/zk-file-cloud'
+import ZkFileIcon from '@/components/zk-file-icon'
+import ZkFileImage from '@/components/zk-file-image'
+import ZkFileVideo from '@/components/zk-file-video'
 import ZkFooter from '@/components/zk-footer'
 import ZkGlobalSetting from '@/components/zk-global-setting'
 import ZkLeft from '@/components/zk-left'
 import ZkPageSetting from '@/components/zk-page-setting'
+import ZkPopupLayout from '@/components/zk-popup-layout'
 import ZkRight from '@/components/zk-right'
 import ZkRightProperty from '@/components/zk-right-property'
 import ZkSafeColor from '@/components/zk-safe-color'
@@ -31,10 +37,16 @@ import XScreenfull from '@/elements/x-screenfull'
 Vue.component('zk-auto-form', ZkAutoForm)
 Vue.component('zk-dialog', ZkDialog)
 Vue.component('zk-file', ZkFile)
+Vue.component('zk-file-audio', ZkFileAudio)
+Vue.component('zk-file-cloud', ZkFileCloud)
+Vue.component('zk-file-icon', ZkFileIcon)
+Vue.component('zk-file-image', ZkFileImage)
+Vue.component('zk-file-video', ZkFileVideo)
 Vue.component('zk-footer', ZkFooter)
 Vue.component('zk-global-setting', ZkGlobalSetting)
 Vue.component('zk-left', ZkLeft)
 Vue.component('zk-page-setting', ZkPageSetting)
+Vue.component('zk-popup-layout', ZkPopupLayout)
 Vue.component('zk-right', ZkRight)
 Vue.component('zk-right-property', ZkRightProperty)
 Vue.component('zk-safe-color', ZkSafeColor)
