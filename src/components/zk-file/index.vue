cc87ee95cd7fb8e3c28f3b9a0c808b7456291544
Salmonberry <sos2012happy@gmail.com>
2018-07-21 17:32:29 +0800

    upgrate:color

--- src/components/zk-file/index.vue
+++ src/components/zk-file/index.vue
@@ -217,258 +217,263 @@
 </template>
 
 <script>
-  import elDragDialog from '@/directive/el-dragDialog'
-  import { WIDGET_GETLIST_GET, WIDGET_CLASS_GET } from '@/service/api/apiUrl'
-  export default {
-    name: 'layout-left',
-    directives: { elDragDialog },
-    props: ['themePageInfo', 'dialogVisible'],
-    data () {
-      return {
-        moduleSearch: '',
-        viewModel: null,
-        fileClass: '',
-        themePageModel: null, // 站点URL
-        fileClassId: 0, // 文件分类Id
-        showContent: 1,
-        images: [
-          {
-            urls: [
-              { url: require('../../../static/img/iphone8.png') },
-              { url: '' },
-              { url: '' },
-              { url: '' },
-              { url: '' },
-              { url: '' }
-            ]
-          },
-          {
-            urls: [
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' }
-            ]
-          },
-          {
-            urls: [
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' }
-            ]
-          },
-          {
-            urls: [
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' },
-              { url: 'javascript:void(0)' }
-            ]
-          }
-        ],
-        width: 0,
-        url: '',
-        editableTabs: [
-          {
-            index: '1-1',
-            content: '未分类'
-          },
-          {
-            index: '1-2',
-            content: '网站广告图'
-          },
-          {
-            index: '1-3',
-            content: '799'
-          },
-          {
-            index: '1-4',
-            content: '网站装修'
-          }
-        ]
-
-      }
-    },
-    mounted () {
-      this.init()
-    },
-    methods: {
-      async init () {
-        // 文件、文件分类导入
-        const para = {
-          relationId: this.fileClassId // 根据参数获取列表
+import elDragDialog from '@/directive/el-dragDialog'
+import { WIDGET_GETLIST_GET, WIDGET_CLASS_GET } from '@/service/api/apiUrl'
+export default {
+  name: 'layout-left',
+  directives: { elDragDialog },
+  props: ['themePageInfo', 'dialogVisible'],
+  data () {
+    return {
+      moduleSearch: '',
+      viewModel: null,
+      fileClass: '',
+      themePageModel: null, // 站点URL
+      fileClassId: 0, // 文件分类Id
+      showContent: 1,
+      images: [
+        {
+          urls: [
+            { url: require('../../../static/img/iphone8.png') },
+            { url: '' },
+            { url: '' },
+            { url: '' },
+            { url: '' },
+            { url: '' }
+          ]
+        },
+        {
+          urls: [
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' }
+          ]
+        },
+        {
+          urls: [
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' }
+          ]
+        },
+        {
+          urls: [
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' },
+            { url: 'javascript:void(0)' }
+          ]
         }
-        this.fileClass = await this.$api.get(WIDGET_CLASS_GET, '', 'widget_class')
-        this.viewModel = await this.$api.get(WIDGET_GETLIST_GET, para, 'widget_list')
-      },
-      handleDrag () {
-        this.$refs.select.blur()
-      },
-      addTab (editableTabsValue2) {
+      ],
+      width: 0,
+      url: '',
+      editableTabs: [
+        {
+          index: '1-1',
+          content: '未分类'
+        },
+        {
+          index: '1-2',
+          content: '网站广告图'
+        },
+        {
+          index: '1-3',
+          content: '799'
+        },
+        {
+          index: '1-4',
+          content: '网站装修'
+        }
+      ]
 
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    async init () {
+      // 文件、文件分类导入
+      const para = {
+        relationId: this.fileClassId // 根据参数获取列表
       }
+      this.fileClass = await this.$api.get(WIDGET_CLASS_GET, '', 'widget_class')
+      this.viewModel = await this.$api.get(WIDGET_GETLIST_GET, para, 'widget_list')
+    },
+    handleDrag () {
+      this.$refs.select.blur()
+    },
+    addTab (editableTabsValue2) {
+
     }
   }
+}
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  @import 'src/assets/styles/mixin.scss';
+@import "src/assets/styles/mixin.scss";
 
-  .upload-demo /deep/.el-upload-dragger {
-    // background-color: yellow;
+.upload-demo /deep/.el-upload-dragger {
+  // background-color: yellow;
+  height: 50px;
+  border: none;
+  // line-height: 50px;
+  position: relative;
+  .el-icon-upload {
+    position: absolute;
+    top: -15px;
+    left: 53px;
+    line-height: 0;
+    display: inline-block;
+    padding-bottom: 10px;
+    font-size: 30px;
+    vertical-align: top;
+  }
+}
+.zk-file {
+  padding: 0;
+  .el-dialog__body {
+    padding: 0px;
+  }
+  .module-title {
     height: 50px;
-    border: none;
-    // line-height: 50px;
-    position: relative;
-    .el-icon-upload {
-      position: absolute;
-      top: -15px;
-      left: 53px;
-      line-height: 0;
-      display: inline-block;
-      padding-bottom: 10px;
-      font-size: 30px;
-      vertical-align: top;
+    line-height: 50px;
+    color: #3899ed;
+    padding-left: 20px;
+    font-size: 18px;
+  }
+  .module-nav {
+    height: 50px;
+    border-top: 1px solid #d4ecfe;
+    border-bottom: 1px solid #d4ecfe;
+    background: #ebf7ff;
+    ul {
+      li {
+        float: left;
+        height: 50px;
+        line-height: 50px;
+        color: #0b5672;
+        padding: 0 40px;
+        border-right: 1px solid $--border-color-extra-light;
+        cursor: pointer;
+      }
+      li.active {
+        background: #fff;
+      }
     }
   }
-  .zk-file {
-    padding: 0;
-    .el-dialog__body {
-      padding: 0px;
-    }
-    .module-title {
+  .module-search {
+    margin-top: 1px;
+    height: 60px;
+    display: flex;
+    border-bottom: 1px solid $--border-color-extra-light;
+    .search-left {
       height: 50px;
+      flex: 1;
       line-height: 50px;
-      color: #3899ed;
+      color: #7d94a6;
       padding-left: 20px;
-      font-size: 18px;
+      .upload {
+        .upload-demo {
+          height: 100px;
+          .el-upload__tip {
+            position: absolute;
+            top: 113px;
+            left: 330px;
+            // background-color: green;
+          }
+        }
+      }
     }
-    .module-nav {
+    .search-right {
       height: 50px;
-      border-top: 1px solid #d4ecfe;
-      border-bottom: 1px solid #d4ecfe;
-      background: #ebf7ff;
-      ul {
-        li {
-          float: left;
-          height: 50px;
-          line-height: 50px;
-          color: #0b5672;
-          padding: 0 40px;
-          border-right: 1px solid $--border-color-extra-light;
-          cursor: pointer;
-        }
-        li.active {
-          background: #fff;
-        }
-      }
-    }
-    .module-search {
-      margin-top: 1px;
-      height: 60px;
       display: flex;
-      border-bottom: 1px solid $--border-color-extra-light;
-      .search-left {
-        height: 50px;
-        flex: 1;
-        line-height: 50px;
-        color: #7d94a6;
-        padding-left: 20px;
-        .upload {
-          .upload-demo {
-            height: 100px;
-            .el-upload__tip {
-              position: absolute;
-              top: 113px;
-              left: 330px;
-              // background-color: green;
-            }
-          }
-        }
+      align-items: center;
+      padding-right: 5px;
+      position: relative;
+      .el-button {
+        margin-left: 10px;
       }
-      .search-right {
-        height: 50px;
-        display: flex;
-        align-items: center;
-        padding-right: 5px;
-        position: relative;
-        .el-button {
-          margin-left: 10px;
-        }
 
-        .upload {
-          position: absolute;
-          top: 80px;
-          right: 20px;
-        }
+      .upload {
+        position: absolute;
+        top: 80px;
+        right: 20px;
       }
     }
-    .module-content {
+  }
+  .module-content {
+    height: 500px;
+    display: flex;
+    border-bottom: 1px solid $--border-color-extra-light;
+    .content-left {
+      width: 140px;
       height: 500px;
-      display: flex;
-      border-bottom: 1px solid $--border-color-extra-light;
-      .content-left {
-        width: 140px;
-        height: 500px;
-        border-right: 1px solid $--border-color-extra-light;
-        .el-menu-item-group {
-          .el-menu-item {
-            height: 40px;
-            text-align: center;
-            line-height: 40px;
-            border-bottom: 1px solil black;
-            &:first-child {
-              margin-top: -14px;
-            }
+      border-right: 1px solid $--border-color-extra-light;
+      .el-menu-item-group {
+        .el-menu-item {
+          height: 40px;
+          text-align: center;
+          line-height: 40px;
+          border-bottom: 1px solil black;
+          &:first-child {
+            margin-top: -14px;
           }
         }
-        .btn_create {
-        }
       }
-      .content-right {
-        padding: 12px;
-        height: 400px;
-        width: 100%;
-        display: flex;
-        justify-content: space-around;
-        .upload {
-          padding-top: 10px;
-          flex-grow: 2;
-        }
-        .el-row {
-          // background-color: red;
-          padding: 0 10px;
-          width: 800px;
-          .el-col {
+      .btn_create {
+      }
+    }
+    .content-right {
+      padding: 12px;
+      height: 400px;
+      width: 100%;
+      display: flex;
+      justify-content: space-around;
+      .upload {
+        padding-top: 10px;
+        flex-grow: 2;
+      }
+      .el-row {
+        // background-color: red;
+        padding: 0 10px;
+        width: 800px;
+        .el-col {
+          width: auto;
+          .grid-content {
+            padding: 0 10px;
+            padding-top: 10px;
             width: auto;
-            .grid-content {
-              padding: 0 10px;
-              padding-top: 10px;
-              width: auto;
-              span {
-                // overflow: hidden;
-                position: relative;
-                display: inline-block;
-                border: 1px solid #cccccc;
-                img {
-                  display: block;
-                  width: 105px;
-                  height: 105px;
-                }
-                .card-management {
-                  cursor: pointer;
-                  display: none;
+            span {
+              // overflow: hidden;
+              position: relative;
+              display: inline-block;
+              border: 1px solid #cccccc;
+              img {
+                display: block;
+                width: 105px;
+                height: 105px;
+              }
+              .card-management {
+                cursor: pointer;
+                display: none;
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 105px;
+                height: 105px;
+                background-color: rgba($--background-color-base, 0.3);
+                .card-mask {
                   position: absolute;
-                  top: 0;
                   left: 0;
-<<<<<<< HEAD
                   bottom: 0;
                   background: $--border-color-lighter;
                   width: 100%;
@@ -481,86 +486,69 @@
                     color: $--color-text-primary;
                     &:hover {
                       color: #0b5672;
-=======
-                  width: 105px;
-                  height: 105px;
-                  background-color: rgba($--background-color-base, 0.3);
-                  .card-mask {
-                    position: absolute;
-                    left: 0;
-                    bottom: 0;
-                    background: $--border-color-lighter;
-                    width: 100%;
-                    height: 30px;
-                    line-height: 30px;
-                    display: flex;
-                    justify-content: space-around;
-                    border-bottom: 2px solid $--border-color-hover;
-                    a {
-                      color: $--color-text-primary;
->>>>>>> 18f1caf26d1079ba862ac60a4987115dd0db9739
                     }
                   }
                 }
-                &:hover .card-management {
-                  display: block;
-                }
+              }
+              &:hover .card-management {
+                display: block;
               }
             }
           }
         }
+      }
 
-        .zk-paginations {
-          position: relative;
-          height: 50px;
-          .pagination {
-            position: absolute;
-            top: 22px;
-            left: 8%;
-          }
+      .zk-paginations {
+        position: relative;
+        height: 50px;
+        .pagination {
+          position: absolute;
+          top: 22px;
+          left: 8%;
         }
-        .content-box {
-          padding: 4px;
-          height: 228px;
-          overflow: hidden;
-          .el-card__body {
-            float: left;
-            width: 260px;
-            height: 200px;
-            border-radius: 5px;
-            border: 1px solid #f3f3f3;
-            margin-right: 10px;
-            margin-bottom: 10px;
-            width: 100%;
-            height: 500px;
-            overflow: auto;
-          }
-          img {
-            width: 100%;
-            height: 150px;
-            border-bottom: 2px solid #f1f1f1;
-          }
-          .time {
-            font-size: 13px;
-            color: #999;
-          }
-          .bottom {
-            margin-top: 13px;
-            line-height: 12px;
-            padding: 0;
-            float: right;
-          }
-          .clearfix:before,
-          .clearfix:after {
-            display: table;
-            content: '';
-          }
-          .clearfix:after {
-            clear: both;
-          }
+      }
+      .content-box {
+        padding: 4px;
+        height: 228px;
+        overflow: hidden;
+        .el-card__body {
+          float: left;
+          width: 260px;
+          height: 200px;
+          border-radius: 5px;
+          border: 1px solid #f3f3f3;
+          margin-right: 10px;
+          margin-bottom: 10px;
+          width: 100%;
+          height: 500px;
+          overflow: auto;
+        }
+        img {
+          width: 100%;
+          height: 150px;
+          border-bottom: 2px solid #f1f1f1;
+        }
+        .time {
+          font-size: 13px;
+          color: #999;
+        }
+        .bottom {
+          margin-top: 13px;
+          line-height: 12px;
+          padding: 0;
+          float: right;
+        }
+        .clearfix:before,
+        .clearfix:after {
+          display: table;
+          content: "";
+        }
+        .clearfix:after {
+          clear: both;
         }
       }
     }
   }
+}
 </style>
 
